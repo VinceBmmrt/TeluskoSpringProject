1a8681c47381785b6b05502f85d640d308967f96
VinceBmmrt <vincebommert75@gmail.com>
2025-05-09 14:15:25 +0200

    Example of Java based configuration replacing XML config

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -1,17 +1,28 @@
 package com.telusko;
 
 
+import com.telusko.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = context.getBean("alien", Alien.class);
-        obj.setAge(21);
-        System.out.println(obj.getAge());
-        obj.code();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean(Desktop.class);
+
+        dt.compile();
+
+
+//        ** XML CONFIG **
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Alien obj = context.getBean("alien", Alien.class);
+//        obj.setAge(21);
+//        System.out.println(obj.getAge());
+//        obj.code();
 
 //        Computer com = context.getBean(Computer.class);
 //        Desktop obj1 = context.getBean("com", Desktop.class);
