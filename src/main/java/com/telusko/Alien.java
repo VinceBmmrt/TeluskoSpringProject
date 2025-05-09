327387534954a96d8790d3132acd2e097d56e8e0
VinceBmmrt <vincebommert75@gmail.com>
2025-05-09 16:14:23 +0200

    Scope annotation and Value annotation

--- src/main/java/com/telusko/Alien.java
+++ src/main/java/com/telusko/Alien.java
@@ -2,13 +2,15 @@ package com.telusko;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
 
 @Component
-
 public class Alien {
+
+    @Value("25")
     private int age;
 
     @Autowired
